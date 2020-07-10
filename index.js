ef407082eaafc37c6aacebd722b337cc5e356b47
Nacho Islas <nacho.th.21@gmail.com>
2020-07-10 15:17:34 -0300

    Nros Complejos
    Calculo de modulo y angulo de un nro complejo y se muestra en un servidor

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var readlineSync = require('readline-sync');
-var Complex = require('complex.js');
+let Complex = require('complex.js');
 const express = require('express')
 const app = express();
 
-let nroComplejo = readlineSync.question('Ingrese un numero complejo(formato (xx)+(yy)j) ');
+let nroComplejo = readlineSync.question('Ingrese un numero complejo(formato (xx)+(yy)i) ');
+var c = new Complex(nroComplejo);
 
+let mensaje=('El modulo es '+(c.abs()).toString()+'y el angulo es'+(c.arg()).toString());
 
 app.get('/', function (req, res) {
-  res.send('El modulo de su numero complejo es ',abs(nroComplejo),'y su fase es ',arg(nroComplejo))
+    res.send(mensaje)
 })
- 
+console.log('Su rta estara en su servidor port:3000');
 app.listen(3000);
